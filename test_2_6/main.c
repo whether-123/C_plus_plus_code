7d57a04a7552822de1f7484f7d8bcd4381099ec8
whether00 <3339554829@qq.com>
2024-02-06 20:32:33 +0800

    模拟进度条。

---
+++ test_2_6/main.c
@@ -0,0 +1,7 @@
+#include"process.h"
+
+int main()
+{
+    ProncessOn();
+    return 0;
+}
---
+++ test_2_6/process.h
@@ -0,0 +1,9 @@
+#pragma once     
+#include<stdio.h>
+#include<string.h>
+#include<unistd.h>
+
+#define NUM 101
+#define S_NUM 5
+
+extern void ProncessOn(); //函数的声明
