f08602d78ebc721d6f6a6a9c5388ef76f0be9e72
whether00 <3339554829@qq.com>
2025-01-12 20:34:46 +0800

    二进制求和(修改后)

---
+++ test_25_1_11.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    void minadd(char i,char j,char& c,string& temp)
+    {
+        if((c-'0')+(i-'0')+(j-'0')>=2)  //进位+本位和 大于2时
+        {
+            temp.insert(temp.begin(), (c-'0')+(i-'0')+(j-'0')-2+'0');
+            c='1';
+        }
+        else   //进位+本位和 小于2时
+        {
+            temp.insert(temp.begin(), (c-'0')+(i-'0')+(j-'0')+'0');
+            c='0';
+        }
+    }
+
+    string addBinary(string a, string b) {
+         
+         string::reverse_iterator ita=a.rbegin();  //反向迭代器
+         string::reverse_iterator itb=b.rbegin();
+         char c='0';   //初始进位为0
+         string temp;
+
+         while(ita!=a.rend()||itb!=b.rend())
+         {
+             if (ita == a.rend()) //其中一个走完后，后续相加默认为0
+                minadd('0', *(itb++), c, temp);
+            else if (itb == b.rend())
+                minadd(*(ita++), '0', c, temp);
+            else
+                minadd(*(ita++), *(itb++), c, temp);  //后置++ 
+         }
+         
+         if(c=='1')  //都走完后 存在进位1 加上
+           temp.insert(temp.begin(), '1');
+
+         return temp;
+    }
+};
